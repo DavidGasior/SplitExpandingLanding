83fb07d2599e4a1702bd06fbe733591613feff20
DavidGasior <david.gasior@gmail.com>
2022-05-10 10:33:58 +0200

    Removed unnecessary code + fixed a typo in JS file(bottomPart).

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const topPart = document.querySelector(".top");
-const Part = document.querySelector(".bottom");
+const bottomPart = document.querySelector(".bottom");
 const mainDiv = document.querySelector(".main-div");
 
 console.log(topPart);
 topPart.addEventListener("mouseenter", () => {
-  console.log(1);
   topPart.classList.add("hover");
   mainDiv.classList.add("hover");
 });
 topPart.addEventListener("mouseleave", () => {
-  console.log(2);
   topPart.classList.remove("hover");
   mainDiv.classList.remove("hover");
 });
 
 bottomPart.addEventListener("mouseenter", () => {
-  console.log(3);
   bottomPart.classList.add("hover");
   mainDiv.classList.add("hover");
 });
 bottomPart.addEventListener("mouseleave", () => {
-  console.log(4);
   bottomPart.classList.remove("hover");
   mainDiv.classList.remove("hover");
 });
